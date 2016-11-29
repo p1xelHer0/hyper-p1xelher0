df935b3e00a0155368b4baad3c70a66b66bbff87
Pontus Nagy <pontusnagy@gmail.com>
2016-11-29 21:42:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# hyper-p1xelher0
+
+A p1xelHer0 theme for hyper.
+
+## Screenshots
+
+![Screenshot]()
+
+## Installation
+
+Edit `~/.hyper.js` and add `'hyper-p1xelher0'` to `plugins: [...]`:
+```js
+module.exports = {
+
+  //...
+
+  plugins: [
+    //... ,
+    'hyper-p1xelher0'
+  ],
+  
+  // ...
+}
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+exports.decorateConfig = (config) => Object.assign({}, config,
+  {
+    foregroundColor: '#cbbca7', // beige
+    backgroundColor: '#332f28', // brown
+    cursorColor:     '#bdaae7', // purple
+    colors: {
+      black:         '#332f28', // brown
+      red:           '#d25552', // red
+      yellow:        '#fca465', // orange
+      green:         '#a1bf7e', // green
+      cyan:          '#67c4a5', // turquoise
+      blue:          '#80bbd7', // blue
+      magenta:       '#bdaae7', // purple
+      white:         '#cbbca7', // beige
+      lightBlack:    '#332f28',
+      lightRed:      '#d25552',
+      lightYellow:   '#fca465',
+      lightGreen:    '#a1bf7e',
+      lightCyan:     '#67c4a5',
+      lightBlue:     '#80bbd7',
+      lightMagenta:  '#bdaae7',
+      lightWhite:    '#cbbca7'
+    }
+  }
+)
